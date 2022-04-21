fcc20fe795074a3fc0012f34e6edf456c6ce6b7b
Rzt Prog <rztprog@outlook.fr>
2022-04-21 17:38:43 +0200

    Add faker gem and seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "faker"
+
+# Run rails db:setup
+
+puts "
+▄▄▄  ·▄▄▄▄•▄▄▄▄▄▄ ▄▄▄·▄▄▄         ▄▄ • 
+▀▄ █·▪▀·.█▌▀•██ ▀▐█ ▄█▀▄ █· ▄█▀▄ ▐█ ▀ ▪
+▐▀▀▄ ▄█▀▀▀•  ▐█.▪ ██▀·▐▀▀▄ ▐█▌.▐▌▄█ ▀█▄
+▐█•█▌█▌▪▄█▀  ▐█▌·▐█▪·•▐█•█▌▐█▌.▐▌▐█▄▪▐█
+.▀  ▀·▀▀▀ •  ▀▀▀ .▀   .▀  ▀ ▀█▄▀▪·▀▀▀▀ 
+"
+
+puts "
+░▒█░░▒█░░▀░░█░▄░░▀░░█▀▀▄░░▀░░█▀▄▀█░▄▀▀▄░█░▒█░█▀▀
+░▒█▒█▒█░░█▀░█▀▄░░█▀░█░▒█░░█▀░█░▀░█░█░░█░█░▒█░▀▀▄
+░▒▀▄▀▄▀░▀▀▀░▀░▀░▀▀▀░▀░░▀░▀▀▀░▀░░▒▀░░▀▀░░░▀▀▀░▀▀▀
+"
+
+# Cleaning all DB model
+puts "=============================="
+puts "Cleaning Article database..."
+Article.destroy_all
+puts "Done"
+puts "=============================="
+
+# Create users
+puts "Creating some post..."
+puts "How much posts you want ?"
+puts "> "
+loop_n = STDIN.gets.chomp
+
+puts "Creating #{loop_n} fake tasks, please wait..."
+loop_n.to_i.times do
+  article = Article.new(
+    title:    "#{Faker::Verb.base.capitalize} #{Faker::ProgrammingLanguage.name}",
+    content:  Faker::Lorem.paragraph_by_chars(number: rand(256..1024), supplemental: false)
+  )
+  article.save!
+end
+
+puts "Congrats, #{Article.count} post(s) was created!"
+puts "=============================="
